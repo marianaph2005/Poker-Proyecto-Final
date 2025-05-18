fb8966eebf012233beadedc50f9d7b23837047c9
Mariana <portillo.mariana@uabc.edu.mx>
2025-05-18 02:08:10 -0700

    Se creó la clase Padre abstractta JuegoPoker con sus atributos protegidos, se escribieron ciertos métodos base así como abstractos para que las subclases los implementen conforme a sus reglas.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         Mazo mazo = new Mazo();
         mazo.mostrar();
         mazo.barajear();
         mazo.mostrar();
+        System.out.println("Mano");
+        ArrayList<Carta> mano = mazo.sacarNCartas(5);
+        Mano mano1 = new Mano(mano);
+        mano1.mostrar();
+        System.out.println(mano1.obtenerCartaAlta());
+
     }
 }
